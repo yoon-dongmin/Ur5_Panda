96f4f88039af64e097f39eb3f4be36c376ad8de0
Dongmin <ehdals6034@naver.com>
2022-06-13 06:59:24 +0000

    0613

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /urpanda_ws/src
+# Build directory: /urpanda_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ur5/ur5_description")
+subdirs("ur5/ur5_gazebo")
+subdirs("urpanda_moveit")
---
+++ build/urpanda_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /urpanda_ws/src/urpanda_moveit
+# Build directory: /urpanda_ws/build/urpanda_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
