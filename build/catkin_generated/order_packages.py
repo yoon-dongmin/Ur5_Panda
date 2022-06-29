c16c64fb5e563ad2e0e3dd189fcf60e48e459cce
Dongmin <ehdals6034@naver.com>
2022-06-29 04:59:29 +0000

    0629

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/urpanda_ws/src'
+source_root_dir = '/ur5/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/ros_ws/devel;/opt/ros/melodic'.split(';') if '/ros_ws/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/ur5/devel;/ros_ws/devel;/opt/ros/melodic'.split(';') if '/ur5/devel;/ros_ws/devel;/opt/ros/melodic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/urpanda_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/urpanda_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/urpanda_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PWD='/urpanda_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/urpanda_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/urpanda_ws/src:$ROS_PACKAGE_PATH"
+export PWD='/ur5/build'
+export ROS_PACKAGE_PATH='/urpanda_ws/src:/ur5/src:/ros_ws/src:/opt/ros/melodic/share'
